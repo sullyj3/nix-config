14e4a2dfa769cccad98515f477a029020e7c262c
James Sully <sullyj3@gmail.com>
2021-12-23 21:38:32 +1100

    Reorganize home.nix

--- home.nix
+++ home.nix
@@ -3,38 +3,60 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "james";
-  home.homeDirectory = "/home/james";
+  home = {
+    username = "james";
+    homeDirectory = "/home/james";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+
+    sessionVariables = {
+      EDITOR = "nvim";
+
+      # TODO what if we're on WSL? not sure how to handle this correctly
+      # more generally this is the reason I haven't started moving gui apps into nix yet
+      BROWSER = "google-chrome-stable";
+      MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    };
+
+    sessionPath = [
+      "/home/james/.cabal/bin"
+      "/home/james/.ghcup/bin"
+    ];
+
+    packages = with pkgs; [
+      du-dust
+      glow
+      neovim
+      ripgrep
+      fd
+      tree
+      dua
+
+      cheat
+      tealdeer
+    ];
+  };
 
   # This is a good idea according to https://nixos.wiki/wiki/Home_Manager
   # the page doesn't really elaborate much as to why
   targets.genericLinux.enable = true;
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-
-    # TODO what if we're on WSL? not sure how to handle this correctly
-    # more generally this is the reason I haven't started moving gui apps into nix yet
-    BROWSER = "google-chrome-stable";
-    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-  };
-
-  home.sessionPath = [
-    "/home/james/.cabal/bin"
-    "/home/james/.ghcup/bin"
-  ];
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
   programs = {
+    broot = {
+      enable = true;
+      enableFishIntegration = true;
+    };
     git = {
       enable = true;
       userName  = "James Sully";
@@ -83,27 +105,10 @@
     htop.enable = true;
   };
 
-  xdg.configFile."cheat/conf.yml".source = ./config/cheat/conf.yml;
-
-  xdg.configFile."starship.toml".source = ./config/starship.toml;
-
-  # fish config
-  xdg.configFile."fish/functions".source = ./config/fish/functions;
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  home.packages = with pkgs; [
-    du-dust
-    glow
-    neovim
-    ripgrep
-    fd
-    tree
-    dua
-
-    cheat
-    tealdeer
-  ];
-
-  xdg.configFile."nvim/init.lua".source = ./config/nvim/init.lua;
+  xdg.configFile = {
+    "cheat/conf.yml".source = ./config/cheat/conf.yml;
+    "starship.toml".source = ./config/starship.toml;
+    "fish/functions".source = ./config/fish/functions;
+    "nvim/init.lua".source = ./config/nvim/init.lua;
+  };
 }
