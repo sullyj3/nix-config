97df54e6eeed1cf31f9b3f64325935409ed29bdc
James Sully <sullyj3@gmail.com>
2022-04-03 10:13:39 +1000

    Add keyboard brightness alias (doesn't work yet)

--- dorian.nix
+++ dorian.nix
@@ -6,5 +6,11 @@
   home = {
     username = "james";
     homeDirectory = "/home/james";
+    shellAliases = {
+      # Set keyboard backlight brightness (0,1,2)
+      # Todo: this alias isn't being set for some reason
+      # investigate how `imports` (above) works
+      kbright = "brightnessctl --device='dell::kbd_backlight' set";
+    };
   };
 }
