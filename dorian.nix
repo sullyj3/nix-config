843bda2fd79dcd7f60cfc6a87c24af6e31752e28
James Sully <sullyj3@gmail.com>
2022-04-03 20:42:39 +1000

    Remove old comment

--- dorian.nix
+++ dorian.nix
@@ -8,8 +8,6 @@
     homeDirectory = "/home/james";
     shellAliases = {
       # Set keyboard backlight brightness (0,1,2)
-      # Todo: this alias isn't being set for some reason
-      # investigate how `imports` (above) works
       kbright = "brightnessctl --device='dell::kbd_backlight' set";
     };
   };
