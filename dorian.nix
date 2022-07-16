fb981e7358d37033913b18a817d6d70a218295c9
James Sully <sullyj3@gmail.com>
2022-07-16 19:21:24 +1000

    Switch to home-manager 22.11

--- dorian.nix
+++ dorian.nix
@@ -4,8 +4,6 @@
   imports = [ ./common.nix ./genericLinux.nix ./guiLinux.nix ];
 
   home = {
-    username = "james";
-    homeDirectory = "/home/james";
     shellAliases = {
       # Set keyboard backlight brightness (0,1,2)
       kbright = "brightnessctl --device='dell::kbd_backlight' set";
--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     unison.url = "github:ceedubs/unison-nix";
@@ -14,17 +14,24 @@
   outputs = { self, nixpkgs, home-manager, unison }:
     let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
       username = "james";
     in {
       homeConfigurations.dorian = home-manager.lib.homeManagerConfiguration {
-        configuration = {
-          imports = [ ./dorian.nix ];
-          nixpkgs.overlays = [ unison.overlay ];
-        };
+        inherit pkgs;
 
-        inherit system username;
-        homeDirectory = "/home/${username}";
-        stateVersion = "22.05";
+        modules = [ 
+          ./dorian.nix
+          ({
+            nixpkgs.overlays = [ unison.overlay ];
+            home = {
+              inherit username;
+              stateVersion = "22.11";
+              homeDirectory = "/home/${username}";
+            };
+          })
+        ];
+
 
       };
     };
