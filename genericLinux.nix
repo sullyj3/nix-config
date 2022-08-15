3a23c8f9b7100f5e9c3e380adb8871a343f3ff76
James Sully <sullyj3@gmail.com>
2022-08-15 20:23:01 +1000

    Add nvm.fish (not working correctly, see comment)

--- genericLinux.nix
+++ genericLinux.nix
@@ -9,6 +9,12 @@
 
   # TODO extract to non nixOS module
   # TODO What the heck is this thing for again?
+
+  # TODO we are currently setting $XDG_CONFIG_HOME/fish/functions to a nix 
+  # store path in common.nix. It seems like this is making plugins that install
+  # functions (like nvm.fish) silently fail.
+  # may have to refactor to use programs.fish.functions
+  # this is a little irritating, since we won't get syntax highlighting
   programs.fish.plugins = [{
     name = "nix-env";
     src = pkgs.fetchFromGitHub {
@@ -17,5 +23,14 @@
       rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
       sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
     };
-  }];
+  }
+  { name = "nvm";
+    src = pkgs.fetchFromGitHub {
+      owner = "jorgebucaran";
+      repo = "nvm.fish";
+      rev = "9db8eaf6e3064a962bca398edd42162f65058ae8";
+      sha256 = "1sffqgvqw4vwljrfkf828mp2gxrvvbcwf2v6chj73rd27s5ajh1f";
+    };
+  }
+  ];
 }
