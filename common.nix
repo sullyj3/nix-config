bcfee183ca98508775312ebd1578b632d0760d1d
James Sully <sullyj3@gmail.com>
2022-11-06 10:51:50 +1100

    Update to home-manager with fix for profile issue
    https://github.com/nix-community/home-manager/commit/ccc9164b76e4167873ed819f0fb014635453ec68

--- common.nix
+++ common.nix
@@ -8,18 +8,6 @@
   manual.html.enable = true;
 
   home = {
-    activation.cleanupOldProfile = lib.hm.dag.entryAfter ["writeBoundary"] (
-      ''
-        if [[ -e "$HOME/.nix-profile"/manifest.json ]] ; then
-          nix profile list \
-            | { grep 'home-manager-path$' || test $? = 1; } \
-            | awk -F ' ' '{ print $4 }' \
-            | cut -d ' ' -f 4 \
-            | xargs -t nix profile remove
-        fi
-      ''
-    );
-
     sessionVariables = {
       EDITOR = "nvim";
       MANPAGER = "sh -c 'col -bx | bat -l man -p'";
