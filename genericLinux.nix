daa296dab47770ff19466f83c9bcb7f8452bcb0b
James Sully <sullyj3@gmail.com>
2022-12-09 13:25:35 +1100

    remove nix-env.fish
    It was clashing with programs.fish.enable, which obviates it

--- genericLinux.nix
+++ genericLinux.nix
@@ -15,22 +15,14 @@
   # functions (like nvm.fish) silently fail.
   # may have to refactor to use programs.fish.functions
   # this is a little irritating, since we won't get syntax highlighting
-  programs.fish.plugins = [{
-    name = "nix-env";
-    src = pkgs.fetchFromGitHub {
-      owner = "lilyball";
-      repo = "nix-env.fish";
-      rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
-      sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
-    };
-  }
-  { name = "nvm";
-    src = pkgs.fetchFromGitHub {
-      owner = "jorgebucaran";
-      repo = "nvm.fish";
-      rev = "9db8eaf6e3064a962bca398edd42162f65058ae8";
-      sha256 = "1sffqgvqw4vwljrfkf828mp2gxrvvbcwf2v6chj73rd27s5ajh1f";
-    };
-  }
+  programs.fish.plugins = [
+    { name = "nvm";
+      src = pkgs.fetchFromGitHub {
+        owner = "jorgebucaran";
+        repo = "nvm.fish";
+        rev = "9db8eaf6e3064a962bca398edd42162f65058ae8";
+        sha256 = "1sffqgvqw4vwljrfkf828mp2gxrvvbcwf2v6chj73rd27s5ajh1f";
+      };
+    }
   ];
 }
