8abf5b5ba153aecc33d8a7ed749f367522fa620f
James Sully <sullyj3@gmail.com>
2023-07-29 22:30:04 +1000

    use recommended packer bootstrap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,19 @@
 -- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+		print("Cloning packer.nvim...")
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		print("Installing packer.nvim...")
+    vim.cmd [[packadd packer.nvim]]
+		print("Packer installed!")
+    return true
+  end
+  return false
 end
+
+ensure_packer()
 
 vim.cmd [[
   augroup Packer
@@ -12,8 +22,7 @@ vim.cmd [[
   augroup end
 ]]
 
-local use = require('packer').use
-require('packer').startup(function()
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
   use 'tpope/vim-fugitive' -- Git commands in nvim
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
