583fd6c81682f7c448d76a32a8842edfe1381264
James Sully <sullyj3@gmail.com>
2023-07-30 12:14:40 +1000

    Move some stuff from guilinux to dorian

--- dorian.nix
+++ dorian.nix
@@ -6,13 +6,18 @@
   imports = [ ./home.nix ./genericLinux.nix ./guiLinux.nix ];
 
   home = {
+    sessionVariables = {
+      BROWSER = "google-chrome-stable";
+    };
     shellAliases = {
       # Set keyboard backlight brightness (0,1,2)
       kbright = "brightnessctl --device='dell::kbd_backlight' set";
+      feh = "feh --draw-filename --force-aliasing --auto-zoom --sort filename --version-sort";
     };
     packages = with pkgs; [
       # doesn't seem to work on WSL, leave it here for now
       nodejs_20
+      signal-desktop
     ];
   };
 }
--- guiLinux.nix
+++ guiLinux.nix
@@ -5,16 +5,11 @@
 {
   home = {
     sessionVariables = {
-      BROWSER = "google-chrome-stable";
       NEOVIDE_MULTIGRID = "true";
     };
 
-    packages = with pkgs; [
-      signal-desktop
-    ];
     shellAliases = {
       win = "alacritty --working-directory . &; disown";
-      feh = "feh --draw-filename --force-aliasing --auto-zoom --sort filename --version-sort";
       nv = "neovide";
     };
   };
