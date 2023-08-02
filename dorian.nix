0452d241b80578bbf33b0052edbef6f201aac935
James Sully <sullyj3@gmail.com>
2023-08-02 11:39:30 +1000

    add commented jujutsu

--- dorian.nix
+++ dorian.nix
@@ -13,6 +13,10 @@
       # Set keyboard backlight brightness (0,1,2)
       kbright = "brightnessctl --device='dell::kbd_backlight' set";
       feh = "feh --draw-filename --force-aliasing --auto-zoom --sort filename --version-sort";
+
+      # jujutsu
+      js = "jj status";
+      jl = "jj log";
     };
     packages = with pkgs; [
       # doesn't seem to work on WSL, leave it here for now
@@ -21,6 +25,18 @@
     ];
   };
 
-  # programs.nushell.enable = true;
-  # programs.zoxide.enableNushellIntegration = true;
+  programs = {
+    # jujutsu = {
+    #   enable = true;
+    #   enableFishIntegration = true;
+      # settings = {
+      #   user.name = "James Sully";
+      #   user.email = "sullyj3@gmail.com";
+      #   ui.default-command = "log";
+      # };
+    # };
+    # nushell.enable = true;
+    # zoxide.enableNushellIntegration = true;
+  };
+
 }
