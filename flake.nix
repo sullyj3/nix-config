f550bf884bb8ce171897f2e8f614bfbbc12f79c0
James Sully <sullyj3@gmail.com>
2023-08-03 15:54:29 +1000

    move mkLinuxHomeConfig into home-configs/default.nix

--- flake.nix
+++ flake.nix
@@ -18,31 +18,8 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      mkLinuxHomeConfig = { imports }: home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = { inherit nixpkgs; };
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = imports ++ [ 
-          ({
-            nixpkgs.overlays = with inputs; [ 
-              yyp.overlays.default
-              jj.overlays.default
-            ];
-            home = 
-              let 
-                username = "james";
-              in
-              {
-                username = username;
-                stateVersion = "23.11";
-                homeDirectory = "/home/${username}";
-              };
-          })
-        ];
-      };
-
-    in {
-      nixosConfigurations = (import ./nixos-configs) { inherit nixpkgs; };
-      homeConfigurations = (import ./home-configs) { inherit mkLinuxHomeConfig; };
+    {
+      nixosConfigurations = import ./nixos-configs { inherit nixpkgs; };
+      homeConfigurations = import ./home-configs inputs;
     };
 }
--- home-configs/default.nix
+++ home-configs/default.nix
@@ -1,4 +1,28 @@
-{ mkLinuxHomeConfig }:
+{ nixpkgs, home-manager, ... }@inputs:
+let
+  mkLinuxHomeConfig = { imports }: home-manager.lib.homeManagerConfiguration {
+    extraSpecialArgs = { inherit nixpkgs; };
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    modules = imports ++ [ 
+      ({
+        nixpkgs.overlays = with inputs; [ 
+          yyp.overlays.default
+          jj.overlays.default
+        ];
+        home = 
+          let 
+            username = "james";
+          in
+          {
+            username = username;
+            stateVersion = "23.11";
+            homeDirectory = "/home/${username}";
+          };
+      })
+    ];
+  };
+
+in
 {
   # Archlabs on laptop
   "james@dorian" = mkLinuxHomeConfig { 
