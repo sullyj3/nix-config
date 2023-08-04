3985cf5fecc4198df327d28773ef58e7a08cdb42
James Sully <sullyj3@gmail.com>
2023-08-04 21:37:01 +1000

    Add comment about polybar bug

--- home-configs/guiLinux.nix
+++ home-configs/guiLinux.nix
@@ -20,6 +20,10 @@ in
 
   xdg.configFile = {
     "i3/config".source = myLib.link (myLib.xdgConf + "/i3/config");
+    # black system tray bug is fixed in unreleased PR 
+    # Issue: polybar/polybar/issues/1995
+    # PR: polybar/polybar/pull/2609 (merged)
+    # to be released in v3.7.0
     "polybar" = {
       source = myLib.xdgConf + "/polybar";
       recursive = true;
