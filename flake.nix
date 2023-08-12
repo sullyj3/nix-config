8e13e99aebbd26141a2f2bedc02a81fa8a4ac0c7
James Sully <sullyj3@gmail.com>
2023-08-13 09:31:45 +1000

    home-manager fix merged, switch back to master

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/fix/4298";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     yyp = {
