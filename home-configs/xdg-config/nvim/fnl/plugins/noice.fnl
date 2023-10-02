5a84b7aed34f3b03dda42d11ca6aa4b1c3ed2277
James Sully <sullyj3@gmail.com>
2023-10-02 18:29:27 +1100

    things

--- home-configs/xdg-config/nvim/fnl/plugins/noice.fnl
+++ home-configs/xdg-config/nvim/fnl/plugins/noice.fnl
@@ -10,7 +10,8 @@
                 :long_message_to_split true   ; long messages will be sent to a split
                 :inc_rename false             ; enables an input dialog for inc-rename.nvim
                 :lsp_doc_border true}     ; add a border to hover docs and signature help
-      :cmdline {:view :cmdline}}))
+      :cmdline {:view :cmdline}
+      :messages {:view :mini}}))
                 
                 
 {: configure}
--- home-configs/xdg-config/nvim/lua/plugins/noice.lua
+++ home-configs/xdg-config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,6 @@
 -- [nfnl] Compiled from fnl/plugins/noice.fnl by https://github.com/Olical/nfnl, do not edit.
 local noice = require("noice")
 local function configure()
-  return noice.setup({lsp = {override = {["vim.lsp.util.convert_input_to_markdown_lines"] = true, ["vim.lsp.util.stylize_markdown"] = true, ["cmp.entry.get_documentation"] = true}}, presets = {bottom_search = true, command_palette = true, long_message_to_split = true, lsp_doc_border = true, inc_rename = false}, cmdline = {view = "cmdline"}})
+  return noice.setup({lsp = {override = {["vim.lsp.util.convert_input_to_markdown_lines"] = true, ["vim.lsp.util.stylize_markdown"] = true, ["cmp.entry.get_documentation"] = true}}, presets = {bottom_search = true, command_palette = true, long_message_to_split = true, lsp_doc_border = true, inc_rename = false}, cmdline = {view = "cmdline"}, messages = {view = "mini"}})
 end
 return {configure = configure}
