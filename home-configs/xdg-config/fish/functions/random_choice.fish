e58c4ca113c77b0fa9b68082fccabd2d730bce31
James Sully <sullyj3@gmail.com>
2024-03-05 16:58:52 +1100

    better error message for random_choice

--- home-configs/xdg-config/fish/functions/random_choice.fish
+++ home-configs/xdg-config/fish/functions/random_choice.fish
@@ -1,3 +1,8 @@
+# fish
 function random_choice
+    if test (count $argv) -eq 0
+        echo "random_choice: no arguments provided" >&2
+        return 1
+    end
     echo $argv[(random 1 (count $argv))]
 end
