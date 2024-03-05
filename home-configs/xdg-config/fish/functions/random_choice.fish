62d9087b44f742e3eef882605ca18048272556cc
James Sully <sullyj3@gmail.com>
2024-03-05 17:04:10 +1100

    random_choice: support choice from list of length 1

--- home-configs/xdg-config/fish/functions/random_choice.fish
+++ home-configs/xdg-config/fish/functions/random_choice.fish
@@ -1,8 +1,15 @@
 # fish
 function random_choice
-    if test (count $argv) -eq 0
+    switch (count $argv)
+    case 0
         echo "random_choice: no arguments provided" >&2
         return 1
+    case 1
+        # need to handle this case separately because `random` doesn't allow 
+        # the endpoints to be equal
+        echo $argv[1]
+        return
+    case '*'
+        echo $argv[(random 1 (count $argv))]
     end
-    echo $argv[(random 1 (count $argv))]
 end
