9dc5ae331a544970ca5bed5e8cc5d4e69d4267f3
James Sully <sullyj3@gmail.com>
2024-06-13 12:25:29 +1000

    nvim edit config command

--- home-configs/xdg-config/nvim/fnl/commands.fnl
+++ home-configs/xdg-config/nvim/fnl/commands.fnl
@@ -1,1 +1,12 @@
-(vim.api.nvim_create_user_command "Cdhere" "cd %:p:h" { :nargs 0 })
+(λ cd-here []
+  (vim.cmd.cd "%:p:h"))
+
+(vim.api.nvim_create_user_command "Cdhere" cd-here { :nargs 0})
+
+(local config-path "~/nix-config/home-configs/xdg-config/nvim")
+
+(vim.api.nvim_create_user_command "Config" 
+  (λ [] 
+     (vim.cmd.edit (.. config-path "/fnl/options.fnl")) 
+     (vim.cmd.cd config-path))
+  { :nargs 0})
--- home-configs/xdg-config/nvim/lua/commands.lua
+++ home-configs/xdg-config/nvim/lua/commands.lua
@@ -1,2 +1,11 @@
 -- [nfnl] Compiled from fnl/commands.fnl by https://github.com/Olical/nfnl, do not edit.
-return vim.api.nvim_create_user_command("Cdhere", "cd %:p:h", {nargs = 0})
+local function cd_here()
+  return vim.cmd.cd("%:p:h")
+end
+vim.api.nvim_create_user_command("Cdhere", cd_here, {nargs = 0})
+local config_path = "~/nix-config/home-configs/xdg-config/nvim"
+local function _1_()
+  vim.cmd.edit((config_path .. "/fnl/options.fnl"))
+  return vim.cmd.cd(config_path)
+end
+return vim.api.nvim_create_user_command("Config", _1_, {nargs = 0})
