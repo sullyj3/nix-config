949f5f66c36d9a29922917f9339a6deaa216e8ec
James Sully <sullyj3@gmail.com>
2024-06-16 18:19:51 +1000

    remove qemu aarch stuff (causing home-manager build failure somehow)

--- home-configs/basics.nix
+++ home-configs/basics.nix
@@ -28,8 +28,6 @@ in {
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
-    extra-platforms = [ "aarch64-linux" ];
-    extra-sandbox-paths = [ "/usr/bin/qemu-aarch64-static" ];
   };
 
   home = {
