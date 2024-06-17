67908d3504da6d6e456893203b2c0282e8fbb93e
James Sully <sullyj3@gmail.com>
2024-06-18 00:24:47 +1000

    copy in wezterm config

---
+++ home-configs/xdg-config/wezterm/wezterm.fnl
@@ -0,0 +1,9 @@
+(local wezterm (require :wezterm))
+
+{ ; :color_scheme "Everforest Dark (Gogh)"
+  :color_scheme "Kanagawa (Gogh)"
+  :hide_tab_bar_if_only_one_tab true
+  :font_size 11.0
+  :window_decorations :RESIZE}
+ 
+ 
---
+++ home-configs/xdg-config/wezterm/wezterm.lua
@@ -0,0 +1,3 @@
+-- [nfnl] Compiled from wezterm.fnl by https://github.com/Olical/nfnl, do not edit.
+local wezterm = require("wezterm")
+return {color_scheme = "Kanagawa (Gogh)", hide_tab_bar_if_only_one_tab = true, font_size = 11, window_decorations = "RESIZE"}
