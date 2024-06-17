0cce064c40d5380f37fb0e9c654e50f50d042187
James Sully <sullyj3@gmail.com>
2024-06-17 15:38:48 +1000

    niri stuff for phrygian

--- home-configs/phrygian.nix
+++ home-configs/phrygian.nix
@@ -1,7 +1,17 @@
-# Archlabs on laptop
+# NixOS desktop
 
 { config, pkgs, ... }:
 
-{
+let myLib = import ./myLib.nix { inherit config; };
+in {
   imports = [ ./basics.nix ./home.nix ./guiLinux.nix ];
+
+  xdg.configFile = {
+    niri.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/niri");
+    waybar.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/waybar");
+    systemd = {
+      source = (myLib.homeConfigLocation + "/xdg-config/systemd");
+      recursive = true;
+    };
+  };
 }
