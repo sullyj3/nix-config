899d66d4951bed070dd25ada86d8d3958dd12aff
James Sully <sullyj3@gmail.com>
2024-06-18 12:19:30 +1000

    remove neovide, i3, polybar

--- home-configs/guiLinux.nix
+++ home-configs/guiLinux.nix
@@ -1,28 +1,25 @@
 # for linux systems with a gui (ie, no WSL)
 
 { config, pkgs, ... }:
-# todo figure out how to extract this stuff into a module
+
 let
   myLib = import ./myLib.nix { inherit config; };
 in
 {
   home = {
     sessionVariables = {
-      NEOVIDE_MULTIGRID = "true";
       TERMINAL = "wezterm";
     };
 
     shellAliases = {
-      # win = "alacritty --working-directory . &; disown";
-      win = "wezterm start --cwd=.";
-      nv = "neovide";
+      win = "open-wezterm-here";
     };
   };
 
   home.file.".xprofile".source = myLib.link (myLib.dotfiles + "/xprofile");
 
   xdg.configFile = {
-    "i3/config".source = myLib.link (myLib.xdgConf + "/i3/config");
+    # "i3/config".source = myLib.link (myLib.xdgConf + "/i3/config");
     # black system tray bug is fixed in unreleased PR 
     # Issue: polybar/polybar/issues/1995
     # PR: polybar/polybar/pull/2609 (merged)
@@ -33,6 +30,7 @@ in
     #   recursive = true;
     # };
 
-    "niri/config.kdl".source = myLib.link (myLib.homeConfigLocation + "/xdg-config/niri/config.kdl");
+    niri.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/niri");
+    waybar.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/waybar");
   };
 }
--- home-configs/phrygian.nix
+++ home-configs/phrygian.nix
@@ -17,8 +17,6 @@ in
   };
 
   xdg.configFile = {
-    niri.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/niri");
-    waybar.source = myLib.link (myLib.homeConfigLocation + "/xdg-config/waybar");
     systemd = {
       source = (myLib.homeConfigLocation + "/xdg-config/systemd");
       recursive = true;
