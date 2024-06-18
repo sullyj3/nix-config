92e5281a3eeeff209607cce6b3274f95af24aaff
James Sully <sullyj3@gmail.com>
2024-06-18 11:27:07 +1000

    add niri config to guilinux

--- home-configs/guiLinux.nix
+++ home-configs/guiLinux.nix
@@ -27,9 +27,12 @@ in
     # Issue: polybar/polybar/issues/1995
     # PR: polybar/polybar/pull/2609 (merged)
     # to be released in v3.7.0
-    "polybar" = {
-      source = myLib.xdgConf + "/polybar";
-      recursive = true;
-    };
+
+    # "polybar" = {
+    #   source = myLib.xdgConf + "/polybar";
+    #   recursive = true;
+    # };
+
+    "niri/config.kdl".source = myLib.link (myLib.homeConfigLocation + "/xdg-config/niri/config.kdl");
   };
 }
