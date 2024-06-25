7cd84ff76630e0d4a6dd2623a330b580930fcb23
James Sully <sullyj3@gmail.com>
2024-06-25 12:50:51 +1000

    phrygian: add mouse cursor

--- home-configs/phrygian.nix
+++ home-configs/phrygian.nix
@@ -20,6 +20,20 @@ in
     pkgs.waypaper
   ];
 
+  home.pointerCursor = {
+    package = pkgs.numix-cursor-theme;
+    name = "Numix-Cursor-Light";
+    x11.enable = true;
+    gtk.enable = true;
+  };
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = "Numix-Cursor-Light";
+      package = pkgs.numix-cursor-theme;
+    };
+  };
+
   xdg.configFile = {
     systemd = {
       source = (myLib.homeConfigLocation + "/xdg-config/systemd");
