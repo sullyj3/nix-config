f09a6053279efebfda714838a67a5af6c01db7fa
James Sully <sullyj3@gmail.com>
2024-07-30 16:02:11 +1000

    wezterm remove fennel, remove alt-enter binding

--- home-configs/xdg-config/wezterm/wezterm.lua
+++ home-configs/xdg-config/wezterm/wezterm.lua
@@ -1,3 +1,19 @@
--- [nfnl] Compiled from wezterm.fnl by https://github.com/Olical/nfnl, do not edit.
 local wezterm = require("wezterm")
-return {color_scheme = "Kanagawa (Gogh)", hide_tab_bar_if_only_one_tab = true, font_size = 11, window_decorations = "RESIZE"}
+
+local keys = {
+    {
+        key="Enter",
+        mods="ALT",
+        action=wezterm.action.DisableDefaultAssignment
+	}
+}
+
+local config = {
+    color_scheme = "Kanagawa (Gogh)",
+    keys = keys,
+    hide_tab_bar_if_only_one_tab = true,
+    font_size = 11,
+    window_decorations = "RESIZE"
+}
+
+return config
