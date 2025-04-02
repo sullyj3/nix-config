20cfb19a862192f10ad90f19b891d82c42de7a3a
James Sully <sullyj3@gmail.com>
2025-04-03 09:42:54 +1100

    home manager 24.11

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     yyp = {
