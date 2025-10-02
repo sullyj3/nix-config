22827e0515c71aeeafdb177d130f39c9c77f54fd
James Sully <sullyj3@gmail.com>
2025-10-02 21:44:18 +1000

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:sodiboo/niri-flake";
     };
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/64eca23f5bca70e00ae8d65835ba241e9d1638b3";
     systems.url = "github:nix-systems/default";
     home-manager = {
       url = "github:nix-community/home-manager";
